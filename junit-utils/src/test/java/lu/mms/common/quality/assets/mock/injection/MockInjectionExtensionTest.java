9f68bc086d31a29e11ffd110a32899b996c027f8
mmamiah <stephane.mamiah@gmail.com>
2021-10-12 19:43:33 +0200

    configuring test logs.

--- junit-utils/src/test/java/lu/mms/common/quality/assets/mock/injection/MockInjectionExtensionTest.java
+++ junit-utils/src/test/java/lu/mms/common/quality/assets/mock/injection/MockInjectionExtensionTest.java
@@ -58,7 +58,7 @@ class MockInjectionExtensionTest {
      * </code>
      */
     @Test
-    void shouldRebuildTheSutWithUserMockWhenMockNameMatchAndTypeMismatch() {
+    void shouldBuildTheSutWithUserMockWhenMockNameMatchAndTypeMismatch() {
         // Arrange
 
         // Act
